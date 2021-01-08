a9a3b6d93e1dfd1b5b2a62b74d1071abe3e88b99
null <2584456014@qq.com>
2021-01-08 14:17:20 +0800

    Update File.ts

--- File.ts
+++ File.ts
@@ -259,7 +259,7 @@ export class File {
         });
     }
 
-    CopyAll(dir: File, fileFilter?: RegExp[], dirFilter?: RegExp[]) {
+    CopyAllFile(dir: File, fileFilter?: RegExp[], dirFilter?: RegExp[]) {
         let fList = dir.GetAll(fileFilter, dirFilter);
         fList.forEach(f => {
             if (f.IsFile()) {
